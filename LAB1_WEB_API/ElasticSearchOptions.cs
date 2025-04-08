25c072c7d131bc39973e0dc9c477cc564742d245
Paracosm <f1nigan567@gmail.com>
2025-04-08 12:30:58 +0300

    add elasticSearch

---
+++ LAB1_WEB_API/ElasticSearchOptions.cs
@@ -0,0 +1,7 @@
+namespace LAB1_WEB_API;
+
+public class ElasticSearchOptions
+{
+    public string Uri { get; set; }
+    public string DefaultIndex { get; set; }
+}
--- LAB1_WEB_API/Program.cs
+++ LAB1_WEB_API/Program.cs
@@ -1,8 +1,9 @@
 using LAB1_WEB_API;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Caching.Distributed;
 using StackExchange.Redis;
+using Elastic.Clients.Elasticsearch;
+
 var builder = WebApplication.CreateBuilder(args);
 // builder.Services.AddAuthentication("Bearer") // схема аутентификации - с помощью jwt-токенов
 //     .AddJwtBearer();
@@ -10,22 +11,45 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
 #region configurationDatabase
+
 //Данные для подключения
+
 //postgres
 string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 //redis
 string redisConf = builder.Configuration["RedisOptions:Configuration"];
 if (string.IsNullOrEmpty(redisConf))
 {
     throw new InvalidOperationException("Redis configuration is missing or invalid.");
 }
+
+// Elasticsearch
+string esUri = builder.Configuration["ElasticsearchOptions:Uri"];
+if (string.IsNullOrEmpty(esUri))
+{
+    throw new InvalidOperationException("Elasticsearch configuration is missing or invalid.");
+}
+
 #endregion
+
+
+
+
 #region databaseConnections
 // Создание соединения с Redis
+
 var redis = ConnectionMultiplexer.Connect(redisConf);
 var db = redis.GetDatabase();
+
 //соединение с postgres
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(connectionString));
+
+// Elasticsearch client
+var esSettings = new ElasticsearchClientSettings(new Uri(esUri))
+    .DefaultIndex("materials"); // optional
+builder.Services.AddSingleton(new ElasticsearchClient(esSettings));
+
 #endregion
 
 var app = builder.Build();
@@ -56,5 +80,20 @@ app.MapGet("/redis_test", async () =>
     );
 
     return result;
+});
+app.MapGet("/elastic_test", async (ElasticsearchClient esClient) =>
+{
+    var response = await esClient.SearchAsync<dynamic>(s => s
+        .Index("materials")
+        .Query(q => q
+            .Match(m => m
+                .Field("lecture_text") // Поле в виде строки
+                .Query("Использование")
+            )
+        )
+    );
+
+    // Возвращаем сырые JSON-данные
+    return Results.Ok(response.Documents);
 });
 app.Run();
